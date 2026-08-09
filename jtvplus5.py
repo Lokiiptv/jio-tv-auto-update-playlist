6e5f20bae48fedc386bc8a79fc35de2727264352
SAYAN <spal75084@gmail.com>
2026-08-10 00:27:24 +0530

    Update jtvplus5.py

--- jtvplus5.py
+++ jtvplus5.py
@@ -12,7 +12,7 @@ try:
         content = response.read().decode('utf-8')
         
         # Save it to a file
-        with open('playlist.m3u', 'w', encoding='utf-8') as f:
+        with open('jtvplus5.m3u', 'w', encoding='utf-8') as f:
             f.write(content)
             
         print("Successfully fetched the playlist and saved it to 'jtvplus5.m3u'")
