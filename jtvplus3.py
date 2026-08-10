334b5b7c456ae3b200609654d340716538dd78e7
SAYAN <spal75084@gmail.com>
2026-08-10 15:43:46 +0530

    Update jtvplus3.py

--- jtvplus3.py
+++ jtvplus3.py
@@ -1,6 +1,6 @@
 import urllib.request
 import urllib.error
-url = 'https://la.drmlive.net/tp/playlist'
+url = 'https://tataplayyash.streamxlive.workers.dev/'
 # We MUST use a specific User-Agent like OTT Navigator, TiviMate, or okhttp to get the playlist
 headers = {
     'User-Agent': 'OTT Navigator'
